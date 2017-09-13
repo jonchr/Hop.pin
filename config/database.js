2aa5cf607282b93cbcef2e9288511f06d6e9b333
j-webber <Jwebbie@Jamess-MacBook-Pro.local>
2017-09-13 07:47:43 -0400

    fixing passport db

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 // config/database.js
 module.exports = {
     'connection': {
-        'host': 'localhost',
-        'user': 'root',
-        'password': ''
+        'host': "p1us8ottbqwio8hv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        'user': "z91snw0fn36y0hw0",
+        'password': "ntkvx01jri0wxmy2"
     },
-	'database': 'hopin_db',
+	'database': "vlu97ud0tq19pc5u",
     'users_table': 'users'
 };
