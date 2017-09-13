5928c8afe65c6d2b8fde7e1a0c4a798a4df7e195
Jon <jonathan.lee.christensen@gmail.com>
2017-09-13 11:03:37 -0400

    got login working

--- config/database.js
+++ config/database.js
@@ -1,10 +1,21 @@
 // config/database.js
 module.exports = {
+    
+    // Heroku database
+    // 'connection': {
+    //     'host': "p1us8ottbqwio8hv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    //     'user': "z91snw0fn36y0hw0",
+    //     'password': "ntkvx01jri0wxmy2"
+    // },
+    // 'database': "vlu97ud0tq19pc5u",
+    // 'users_table': 'users'
+
+    //Local database
     'connection': {
-        'host': "p1us8ottbqwio8hv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-        'user': "z91snw0fn36y0hw0",
-        'password': "ntkvx01jri0wxmy2"
+        'host': "localhost",
+        'user': "root",
+        'password': ""
     },
-	'database': "vlu97ud0tq19pc5u",
+	'database': "hopin_db",
     'users_table': 'users'
 };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // set up ======================================================================
 var express = require("express");
 var session  = require("express-session");
+var cookieParser = require('cookie-parser');
 var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
 
@@ -10,14 +11,17 @@ var app = express();
 var db = require("./models");
 
 var passport = require("passport");
+var flash    = require('connect-flash');
+
+// configuration ===============================================================
 require('./config/passport')(passport); // pass passport for configuration
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // required for passport
+app.use(cookieParser()); // read cookies (needed for auth)
 app.use(session({
 	secret: "teammodest",
 	resave: true,
@@ -25,17 +29,18 @@ app.use(session({
  } )); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
-
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 // Set Handlebars.
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/Controller.js");
+var routes = require("./controllers/Controller2.js");
 
 app.use("/", routes);
 
 db.sequelize.sync({ force: true}).then(function() {
 	app.listen(port);
+	console.log("Listening on " + port);
 });
