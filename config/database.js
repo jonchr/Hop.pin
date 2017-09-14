27a84e0d268f31f4b2d982fd649c81c4d3e3b87a
James Webber <jwebbs13@gmail.com>
2017-09-14 12:30:39 -0400

    removing sensitive information

--- config/database.js
+++ config/database.js
@@ -3,9 +3,9 @@ module.exports = {
     
 //     // Heroku database
 //     'connection': {
-//         'host': "p1us8ottbqwio8hv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-//         'user': "z91snw0fn36y0hw0",
-//         'password': "ntkvx01jri0wxmy2"
+//         'host': "",
+//         'user': "",
+//         'password': ""
 //     },
 //     'database': "vlu97ud0tq19pc5u",
 //     'users_table': 'users'
